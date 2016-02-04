0c1b50ce5369173c424db76885d0d2bd017c1879
Orlando Sterling <orlandosterling@storm.cis.fordham.edu>
2016-02-04 11:02:40 -0500

    Hello World!

---
+++ HolloWorld.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+int main ()
+{
+  std::cout << "Hi, my name is Orlando.";
+  std::cout << " I am 30 years old, ";
+  std::cout << "and my favorite thing is soccer. ";   
+  return 0;
+}
